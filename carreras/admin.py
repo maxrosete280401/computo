0160779156109eb831f88e15e11e3fe665256e54
maxrosete280401 <srosetep@toluca.tecnm.mx>
2024-06-18 13:01:30 -0500

    Commit 5 BD

--- carreras/admin.py
+++ carreras/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+
+
+from .models import  Carreras
+# Register your models here.
+admin.site.register(Carreras)
--- mensajes/admin.py
+++ mensajes/admin.py
@@ -1,11 +1,4 @@
 from django.contrib import admin
-
+from .models import  Mensajes
 # Register your models here.
-
-
-from .models import Carreras, Persona, Mensajes
-# Register your models here.
-
-admin.site.register(Carreras)
-admin.site.register(Persona)
 admin.site.register(Mensajes)
